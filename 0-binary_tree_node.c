1a03545dced0b841f0f5e52ed0382634a86c6ba9
Parallels <acarrion_pr@yahoo.com>
2023-01-04 18:44:04 -0400

    fixing code

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -14,6 +14,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	newNode->n = value;
 	newNode->parent = parent;
+	newNode->left = NULL;
+	newNode->right = NULL;
 
 	return (newNode);
 }
