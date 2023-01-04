5f6b0d95eabe4966e4233a778d5d07845f853cf4
Parallels <acarrion_pr@yahoo.com>
2023-01-04 18:38:55 -0400

    uploading header, readme and first file

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+ *binary_tree_node-function to create a binary tree node
+ *@parent: pointer to the parent node of the node to create
+ *@value: value to put in the new node
+ *Return: pointer to the new node created, or NUL on failure
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
+
+	if (newNode == NULL)
+		return (NULL);
+
+	newNode->n = value;
+	newNode->parent = parent;
+	newNode->left = NULL;
+
+	return (newNode);
+}
