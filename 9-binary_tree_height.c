8c55736816043d007ee8e0f3a18919870ab3437b
Parallels <acarrion_pr@yahoo.com>
2023-01-05 17:18:59 -0400

    height of binary tree

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+/**
+ *binary_tree_height-function to measure the height of a binary tree
+ *@tree: pointer to root node
+ *Return: the height of binary tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+	if (left_height > right_height)
+	{
+		return (left_height + 1);
+	}
+	else
+	{
+		return (right_height + 1);
+	}
+}
