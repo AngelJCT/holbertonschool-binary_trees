5073af33b0ca4d3f79c9f408814590403e025e78
Parallels <acarrion_pr@yahoo.com>
2023-01-05 16:04:20 -0400

    adding comments

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -10,9 +10,14 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree == NULL || func == NULL)
 		return;
 
+	/*call itself recursively on the left children and all its descendants, using the same in-order traversal*/
 	binary_tree_inorder(tree->left, func);
 
+	/*call func on the value of the current node*/
 	func(tree->n);
 
+	/*call itself recursively on the right children and all its descendants, using the same in-order traversal*/
 	binary_tree_inorder(tree->right, func);
+
+	/*In-order traversal is a type of tree traversal that visits the left child of the current node, the current node, and then the right child of the current node, until all the nodes have been visited.*/
 }
