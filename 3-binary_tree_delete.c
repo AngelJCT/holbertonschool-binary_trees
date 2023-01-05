53b31dcf29c0e448ac25585e8f856ba1bf082294
Parallels <acarrion_pr@yahoo.com>
2023-01-05 15:06:03 -0400

    adding comments

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -9,6 +9,9 @@ void binary_tree_delete(binary_tree_t *tree)
 	if (tree == NULL)
 		return;
 
+	//call itself recursively on the left and right children of the current
+	//node, and then frees the memory for the current node. This process continues
+	//until all the nodes in the tree have been visited and freed.
 	binary_tree_delete(tree->left);
 	binary_tree_delete(tree->right);
 	free(tree);
