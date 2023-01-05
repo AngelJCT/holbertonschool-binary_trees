ed04e1009e05222b2228efd69fd5323553641e2c
Parallels <acarrion_pr@yahoo.com>
2023-01-05 17:34:00 -0400

    height of binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,13 +6,17 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	size_t left_height = 0, right_height = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	if (tree->left != NULL)
+		left_height = binary_tree_height(tree->left) + 1;
+
+	if (tree->right != NULL)
+		right_height = binary_tree_height(tree->right) + 1;
+
 	if (left_height > right_height)
 	{
 		return (left_height);
