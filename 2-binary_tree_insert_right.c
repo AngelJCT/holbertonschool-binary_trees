1c2d38acf914d3db27b24f55f5a6fdb0924a6c8e
Parallels <acarrion_pr@yahoo.com>
2023-01-04 21:55:47 -0400

    uploading insert right

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+/**
+ *binary_tree_insert_right-function to insert a node as right-child
+ *@parent: pointer to the node to insert the right-child in
+ *@value: value to store in the new node
+ *Return: pointer to the new node, or NULL on failure or if parent is NULL
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newNode;
+
+	if (parent == NULL)
+		return (NULL);
+
+	newNode = malloc(sizeof(binary_tree_t));
+	if (newNode == NULL)
+		return (NULL);
+
+	newNode->n = value;
+	newNode->parent = parent;
+	newNode->left = NULL;
+	newNode->right = NULL;
+
+	if (parent->right != NULL)
+	{
+		newNode->right = parent->right;
+		parent->right->parent = newNode;
+	}
+	parent->right = newNode;
+	return (newNode);
+}
