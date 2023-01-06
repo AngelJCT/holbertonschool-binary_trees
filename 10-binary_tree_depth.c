5ae7a23d63fd029a1f05069d3369308247fa8f6c
Parallels <acarrion_pr@yahoo.com>
2023-01-06 12:22:06 -0400

    adding comments

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -18,3 +18,9 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	}
 	return (depth);
 }
+
+/*The height of a 'node' in a binary tree is the largest number of edges in a path from a leaf node to a target node. The height of a 'binary tree' is the height of the root node in the whole binary tree. In other words, the height of a binary tree is equal to the largest number of edges from the root to the most distant leaf node.*/
+
+/*The depth of a 'node' in a binary tree is the total number of edges from the root node to the target node. The depth of a 'binary tree' is the total number of edges from the root node to the most distant leaf node.*/
+
+/*When calculating the depth of the whole binary tree, its equivalent  the height of a binary tree.*/
