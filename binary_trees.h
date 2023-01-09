e89ce2b4b7a2d75f7456953a8d919640207324d8
Parallels <acarrion_pr@yahoo.com>
2023-01-09 08:25:55 -0400

    binary tree is full

--- binary_trees.h
+++ binary_trees.h
@@ -39,5 +39,6 @@ size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 int height(const binary_tree_t *tree);
 int max(int a, int b);
+int binary_tree_is_full(const binary_tree_t *tree);
 #endif
 
