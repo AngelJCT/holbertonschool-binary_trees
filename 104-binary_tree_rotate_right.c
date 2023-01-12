8edf530446152a7d0f3ceb820df4a1374c0259af
Parallels <acarrion_pr@yahoo.com>
2023-01-12 14:52:02 -0400

    right rotation fix

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -6,16 +6,22 @@
  */
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
-	binary_tree_t *newRoot = NULL;
+	binary_tree_t *new_root = NULL;
 
 	if (tree == NULL)
 		return (NULL);
 
-	if (tree != NULL && tree->left != NULL)
-	{
-		newRoot = tree->left;
-		tree->left = newRoot->right;
-		newRoot->right = tree;
-	}
-	return (newRoot);
+	new_root = tree->left;
+	if (new_root == NULL)
+		return (tree);
+
+	tree->left = new_root->right;
+	if (new_root->right != NULL)
+		new_root->right->parent = tree;
+
+	new_root->right = tree;
+	new_root->parent = tree->parent;
+	tree->parent = new_root;
+
+	return (new_root);
 }
