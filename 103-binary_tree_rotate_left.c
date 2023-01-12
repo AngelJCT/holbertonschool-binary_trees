b2b95137c5d56ea9eac8473f3c3016f97fa4bd19
Parallels <acarrion_pr@yahoo.com>
2023-01-12 13:21:23 -0400

    left-rotation in binary tree

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -6,7 +6,12 @@
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-	binary_tree_t *new_root = tree->right;
+	binary_tree_t *new_root;
+
+	if (tree == NULL)
+		return (NULL);
+
+	new_root = tree->right;
 
 	tree->right = new_root->left;
 
